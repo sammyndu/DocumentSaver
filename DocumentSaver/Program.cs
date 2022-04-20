ac9e5dcdd1c512a60dd518c0a988e7439b75f154
samuel ndu <samndu2@gmail.com>
2022-04-20 07:32:33 +0100

    added swagger

--- DocumentSaver/Program.cs
+++ DocumentSaver/Program.cs
@@ -39,11 +39,14 @@ builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSet
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
